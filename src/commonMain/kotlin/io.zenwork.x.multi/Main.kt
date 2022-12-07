2c33e163accb0272c9233b62fdc96ebb83a6a525
zework <florian@hehlen.name>
2022-12-07 17:11:03 +0100

    added tests; created customizable rule interface

--- src/commonMain/kotlin/io.zenwork.x.multi/Main.kt
+++ src/commonMain/kotlin/io.zenwork.x.multi/Main.kt
@@ -22,7 +22,7 @@ class Calculator : Calc {
     override fun equalz(): Int {
         val eq = result
         result = 0
-        println("equals: $eq")
+        println("equals: [$eq]")
         return eq
     }
 
@@ -31,18 +31,72 @@ class Calculator : Calc {
 @JsExport
 @JsName("Result")
 class Result(
-    @JsName("name")
-    val name: String = "",
+    val rule: String = "",
     val status: Int = 200,
     val details: String = "",
     val message: String = "OK",
-    val originalValue: String? = null
+    val originalValue: Any? = null
 )
+
+@JsExport
+@JsName("Rule")
+abstract class Rule(val name: String) {
+    abstract fun <T> rule(value: T): Result
+}
 
 
 @JsExport
-class Validation {
-    fun validate(name: String, value: String): Result {
-        return Result(name = name, originalValue = value, details = "nothing to do")
+class Validation(private val rules: Array<Rule> = arrayOf()) {
+
+    fun validate(name: String, value: Any?): Result {
+        when {
+            name == "is/string" && value is CharSequence -> {
+                return OK(name, value)
+            }
+
+            name == "is/null" && value == null -> {
+                return OK(name, value)
+            }
+
+            name.startsWith("is/number") && value is Number -> {
+                when {
+                    name == "is/number/positive" && value.toDouble() < 1 -> {
+                        return NOK(name, value)
+                    }
+
+                    name == "is/number/negative" && value.toDouble() > 1 -> {
+                        return NOK(name, value)
+                    }
+
+                    else -> return OK(name, value)
+                }
+
+
+            }
+
+            name.startsWith("is/x") && rules.any { rule -> rule.name == name } -> {
+                println("hi")
+                return rules.first { rule -> rule.name == name }.rule(value)
+            }
+
+            else -> return NOK(name, value)
+        }
+
     }
+
+}
+
+@JsExport
+fun OK(name: String, value: Any?) =
+    Result(rule = name, originalValue = value, details = "nothing to do")
+
+@JsExport
+fun NOK(name: String, value: Any?): Result {
+    return Result(
+        rule = name,
+        originalValue = value,
+        details = "[$value] failed [${name.replace("/x", "").replace(Regex("[/_-]"), " ")}] assertion",
+        status = 500,
+        message = "NOK"
+    )
 }
--- src/commonTest/kotlin/ValidationTest.kt
+++ src/commonTest/kotlin/ValidationTest.kt
@@ -1,3 +1,7 @@
+import io.zenwork.x.multi.NOK
+import io.zenwork.x.multi.OK
+import io.zenwork.x.multi.Result
+import io.zenwork.x.multi.Rule
 import io.zenwork.x.multi.Validation
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -5,12 +9,110 @@ import kotlin.test.assertEquals
 class ValidationTest {
 
     @Test
-    fun validate() {
-        val result = Validation().validate("unknown", "something")
-        assertEquals("unknown", result.name)
+    fun validateString() {
+        val result = Validation().validate("is/string", "something")
+        assertEquals("is/string", result.rule)
         assertEquals(200, result.status)
         assertEquals("OK", result.message)
         assertEquals("something", result.originalValue)
         assertEquals("nothing to do", result.details)
+    }
+
+    @Test
+    fun validateNull() {
+        val result = Validation().validate("is/null", null)
+        assertEquals("is/null", result.rule)
+        assertEquals(null, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+    }
+
+    @Test
+    fun validateNumber() {
+        val result = Validation().validate("is/number", -100)
+        assertEquals("is/number", result.rule)
+        assertEquals(-100, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+    }
+
+    @Test
+    fun validateNotNumber() {
+        val result = Validation().validate("is/number", "abcd")
+        assertEquals("is/number", result.rule)
+        assertEquals("abcd", result.originalValue)
+        assertEquals(500, result.status)
+        assertEquals("NOK", result.message)
+        assertEquals("[abcd] failed [is number] assertion", result.details)
+    }
+
+    @Test
+    fun validatePositiveNumber() {
+        val result = Validation().validate("is/number/positive", 100.5)
+        assertEquals("is/number/positive", result.rule)
+        assertEquals(100.5, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+    }
+
+    @Test
+    fun validateNegativeNumber() {
+        val result = Validation().validate("is/number/negative", -100.5)
+        assertEquals("is/number/negative", result.rule)
+        assertEquals(-100.5, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+    }
+
+    class EstimationPokerRule : Rule("is/x/estimation-poker") {
+        override fun <Number>rule(value: Number): Result {
+            return when (value) {
+                0 -> OK(name, value)
+                1-> OK(name, value)
+                2-> OK(name, value)
+                3-> OK(name, value)
+                5-> OK(name, value)
+                8-> OK(name, value)
+                13-> OK(name, value)
+                else -> NOK(name, value)
+            }
+        }
+    }
+
+    @Test
+    fun validatePokerEstimationNumber() {
+        val validation = Validation(arrayOf(EstimationPokerRule()))
+        var result = validation.validate("is/x/estimation-poker", 0)
+        assertEquals("is/x/estimation-poker", result.rule)
+        assertEquals(0, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+
+        result = validation.validate("is/x/estimation-poker", 8)
+        assertEquals("is/x/estimation-poker", result.rule)
+        assertEquals(8, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+
+        result = validation.validate("is/x/estimation-poker", 13)
+        assertEquals("is/x/estimation-poker", result.rule)
+        assertEquals(13, result.originalValue)
+        assertEquals(200, result.status)
+        assertEquals("OK", result.message)
+        assertEquals("nothing to do", result.details)
+
+        result = validation.validate("is/x/estimation-poker", 11)
+        assertEquals("is/x/estimation-poker", result.rule)
+        assertEquals(11, result.originalValue)
+        assertEquals(500, result.status)
+        assertEquals("NOK", result.message)
+        assertEquals("[11] failed [is estimation poker] assertion", result.details)
+
     }
 }
